c0fab4cd47d58e748679ace673c79aca17115611
Himanshu Mittal <mittalhimanshu121@gmail.com>
2019-02-21 11:02:28 +0530

    rendering template base on uri

--- src/main/java/com/springBasics/giflib/controller/GifController.java
+++ src/main/java/com/springBasics/giflib/controller/GifController.java
@@ -1,22 +1,31 @@
 package com.springBasics.giflib.controller;
 
+import com.springBasics.giflib.data.GifRepository;
 import com.springBasics.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping(value = "/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("android-explosion", LocalDate.of(2019, 2, 23), "Himanshu Mittal", true);
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(ModelMap modelMap, @PathVariable String name) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
--- src/main/java/com/springBasics/giflib/data/GifRepository.java
+++ src/main/java/com/springBasics/giflib/data/GifRepository.java
@@ -1,4 +1,32 @@
 package com.springBasics.giflib.data;
 
+import com.springBasics.giflib.model.Gif;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+        new Gif("android-explosion", LocalDate.of(2019, 2, 23), "Himanshu", false),
+        new Gif("ben-and-mike", LocalDate.of(2017, 6, 25), "Ben", true),
+        new Gif("book-dominos", LocalDate.of(2014, 7, 2), "Mike", false),
+        new Gif("compiler-bot", LocalDate.of(2019, 3, 27), "Compiler", true),
+        new Gif("cowboy-coder", LocalDate.of(2014, 4, 15), "Cowboy", false),
+        new Gif("infinite-andrew", LocalDate.of(5019, 9, 12), "Andrew", true)
+    );
+
+    public Gif findByName(String name){
+        for(Gif gif: ALL_GIFS) {
+            if(gif.getName().equals(name)){
+                return gif;
+            }
+        }
+        return null;
+    }
+
+    public List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
 }
